c7ea65f459de864d9a5c76cd29e97bd3ddff3f81
mpaulson <mpaulson@netflix.com>
2022-08-08 12:38:04 -0600

    feat: i should of had a clarify message
    You can clarify my balls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+use arduino_uno::prelude::*;
+use arduino_uno::hal::port::portb::PB5;
+use arduino_uno::hal::port::mode::Output;
+
 fn main() {
     println!("Hello, world!");
 }
